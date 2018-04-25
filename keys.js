be2e887561c659d862dcf607227da5c5be590912
frankwichw <frankwichw@gmail.com>
2018-04-24 20:14:07 -0400

    keys.js fix

---
+++ keys.js
@@ -0,0 +1,9 @@
+// keys
+var keys = {
+    CONSUMER_KEY: "llaqX51LJMghLTCc5j4nPWPr5",
+    CONSUMER_SECRET: "naT2Rg7308cF1tqq71z1e6FrTIzjvlLj02bezt4tbkBkYcBW8x",
+    ACCESS_TOKEN: "988547768070307841-TLYbGKkBeWGpLM3mhcrdFrHxeWCic6E",
+    ACCESS_TOKEN_SECRET: "kKdhqxRH6cxTEAglH8uuhQTeCzRnotsvGyTCimZNAr7ZP"
+};
+
+export default keys;
